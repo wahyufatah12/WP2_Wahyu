2d8aa7f0eef4ab4a5624fc308b19f42ff1e85608
WahyuFth12 <136737311+wahyufatah12@users.noreply.github.com>
2024-05-07 08:50:24 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+Class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya wahyu fatah
+              Saya tinggal di pondok petir
+              olahraga yang saya suka sukai adalah
+              Berenang";
+    }
+}
